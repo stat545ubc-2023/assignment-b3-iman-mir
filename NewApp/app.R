73ecafc08fb1518279a2ced46d561e6eddf4bfc9
iman-mir <mirmumtahina@gmail.com>
2023-11-27 12:22:06 -0800

    update

--- NewApp/app.R
+++ NewApp/app.R
@@ -1,51 +1,36 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Coffee Recommender"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("rating", "Rating of Coffee:", min = 84, max = 97, value = c(90)),
+      actionButton("recommend", "Get Coffee Recommendation")
+    ),
+    mainPanel(
+      textOutput("recommendation")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+
+
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$recommendation <- renderText({
+    filtered_data <- coffee_data %>%
+      filter(rating == input$rating)
+    
+    if (nrow(filtered_data) > 0) {
+      # If filtered_data is not empty, continue with further operations
+      recommended_coffees <- paste(filtered_data$name)
+      return(recommended_coffees)
+    } else {
+      # If filtered_data is empty, show a message
+      return("Sorry, no coffee matches your criteria.")
+    }
+  })
 }
 
-# Run the application 
+# Run Shiny app
 shinyApp(ui = ui, server = server)
